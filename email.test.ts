a275492e7a6a545eb04afc69a9ca8a432384e650
selin-kavlak-vinci <selin.kavlak@student.vinci.be>
2026-01-16 17:02:12 +0100

    add: should return false when given a@b. (the last char cant be .)

--- email.test.ts
+++ email.test.ts
@@ -1,14 +1,30 @@
 import { Email } from "./email";
 
 describe("Vérification d'adresse d'email", () => {
+  let email = new Email();
   describe("Test du @", () => {
-    it("should return true when given @", () => {
-      //Arrange
-      let email = new Email();
+    it("should return true when given @.be", () => {
       //Act
-      let actual = email.validEmail("@");
+      let actual = email.validEmail("@.be"); //On le modifie pour qu'il passe les nouveaux tests
       //Assert
       expect(actual).toBe(true);
+    });
+    it("should return true when given selin@gmail.com", () => {
+      let actual = email.validEmail("selin@gmail.com");
+
+      expect(actual).toBe(true);
+    });
+  });
+  describe("Test du .", () => {
+    it("should return true when given a@b.com", () => {
+      let actual = email.validEmail("a@b.com");
+
+      expect(actual).toBe(true);
+    });
+    it("should return false when given a@b.", () => {
+      let actual = email.validEmail("a@b.");
+
+      expect(actual).toBe(false);
     });
   });
 });
